423f21a7723abc913f33caa88385c4d375aebbb4
Hafizhnauvaleka <80324531+Rumahweb12@users.noreply.github.com>
2021-10-15 11:03:31 +0700

    Postest2
    postest2

---
+++ akademikk.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Okt 2021 pada 05.52
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademikk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(5) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jkel` varchar(1) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `tgllhr` date DEFAULT NULL,
+  `Kelas` varchar(5) NOT NULL,
+  `Nilai` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`, `Kelas`, `Nilai`) VALUES
+('MHS01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01', 'A', '90'),
+('MHS02', 'Rita', 'P', 'SOLO', '1999-01-01', 'A', '95'),
+('MHS03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11', 'B', '80'),
+('MHS04', 'Siti Maryam', 'L', 'JAKARTA', '1995-04-15', 'D', '85');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
